0df2732d6ed263f9e9b243048383b4f5f7db6628
an9nymous <venezianoalessandro1@gmail.com>
2024-04-28 23:05:49 +0200

    gener

--- progetto/Main.js
+++ progetto/Main.js
@@ -1,2 +1,33 @@
-const gameCanvas = document.getElementById("canvasGioco"); //! salvo in una variabile il canvas
-const gameContext = gameCanvas.getContext("2d"); //! dico il tipo di canvas
+import kaboom from "./libs/kaboom.mjs"
+kaboom({
+    width:1280,
+    height:720,
+    letterbox:true,  //!Tiene fissi i parametri adattandosi alla schermata 
+
+})
+//*livelli
+const scenes= {
+    menu:()=>{ //menù principale
+        // add([text("test"),pos(500,500),color(0,0,0)])
+    },
+    Controls:()=>{
+
+    },
+    1:()=>{
+
+    },
+    2:()=>{
+
+    },
+    gameover:()=>{
+
+    },
+    end:()=>{
+
+    }
+};
+
+for(const key in scenes){
+    scene(key, scenes[key])
+}
+go("menu")
---
+++ progetto/utils/Loader.js
@@ -0,0 +1,8 @@
+export const load = {
+    fonts : () => {
+        loadFont("Round","./asset/Round9x13.ttf")
+    },
+    assets:()=>{
+        loadSprite("forest-background","./asset/Forest_Background_0.png")/** le prime virgolette sono come vogliamo riferirci (come il tag) e le seconde sono il percorso */
+    }
+}
